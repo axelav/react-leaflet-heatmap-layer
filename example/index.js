23ec204146ac9fe013dbd06f9e8827a529e741af
Axel Anderson <axel@pillar.tech>
2019-03-12 16:22:14 -0400

    update example

--- example/index.js
+++ example/index.js
@@ -1,125 +1,138 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Map, TileLayer } from 'react-leaflet';
-import HeatmapLayer from '../src/HeatmapLayer';
-import { addressPoints } from './realworld.10000.js';
+import React from 'react'
+import { render } from 'react-dom'
+import { Map, TileLayer } from 'react-leaflet'
+import HeatmapLayer from '../src/HeatmapLayer'
+import { addressPoints } from './realworld.10000.js'
 
 class MapExample extends React.Component {
-
   state = {
-    mapHidden: false,
-    layerHidden: false,
     addressPoints,
-    radius: 4,
+    radius: 20,
     blur: 8,
-    max: 0.5,
-    limitAddressPoints: true,
-  };
+    max: 0.02,
+    selectedReadingType: 'temp'
+  }
 
   /**
    * Toggle limiting the address points to test behavior with refocusing/zooming when data points change
    */
-  toggleLimitedAddressPoints() {
-    if (this.state.limitAddressPoints) {
-      this.setState({ addressPoints: addressPoints.slice(500, 1000), limitAddressPoints: false });
-    } else {
-      this.setState({ addressPoints, limitAddressPoints: true });
-    }
-  }
 
   render() {
-    if (this.state.mapHidden) {
-      return (
-        <div>
-          <input
-            type="button"
-            value="Toggle Map"
-            onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
-          />
-        </div>
-      );
+    const COLORS = {
+      WHITE: '#ffffff',
+      BLUE: '#357edd',
+      DARK_BLUE: '#00449E',
+      NAVY: '#001B44',
+      GREEN: '#19a974',
+      DARK_GREEN: '#137752',
+      RED: '#FF4136',
+      DARK_RED: '#E7040F',
+      LIGHT_RED: '#FF725C',
+      LIGHT_GREEN: '#9EEBCF',
+      LIGHT_BLUE: '#96CCFF',
+      ORANGE: '#F48120',
+      PURPLE: '#A463F2',
+      YELLOW: '#FFD700',
+      GOLD: '#FFB700',
+      BLACK: '#000000',
+      BLACK_80: 'rgba(0, 0, 0, 0.8)',
+      GRAY: '#777777',
+      MOON_GRAY: '#CCCCCC',
+      LIGHT_GRAY: '#EEEEEE',
+      LIGHT_SILVER: '#AAAAAA',
+      TRANSPARENT: 'transparent'
     }
 
     const gradient = {
-      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
-      0.6: '#FAF3A5', 0.8: '#F5D98B', '1.0': '#DE9A96'
-    };
+      0.0: COLORS.DARK_BLUE,
+      0.25: COLORS.BLUE,
+      0.5: COLORS.YELLOW,
+      0.7: COLORS.ORANGE,
+      0.85: COLORS.RED,
+      '1.0': COLORS.DARK_RED
+    }
 
     return (
       <div>
         <Map center={[0, 0]} zoom={13}>
-          {!this.state.layerHidden &&
-              <HeatmapLayer
-                fitBoundsOnLoad
-                fitBoundsOnUpdate
-                points={this.state.addressPoints}
-                longitudeExtractor={m => m[1]}
-                latitudeExtractor={m => m[0]}
-                gradient={gradient}
-                intensityExtractor={m => parseFloat(m[2])}
-                radius={Number(this.state.radius)}
-                blur={Number(this.state.blur)}
-                max={Number.parseFloat(this.state.max)}
-              />
-            }
+          <HeatmapLayer
+            fitBoundsOnLoad
+            fitBoundsOnUpdate
+            points={this.state.addressPoints}
+            longitudeExtractor={m => m[1]}
+            latitudeExtractor={m => m[0]}
+            gradient={gradient}
+            intensityExtractor={m => {
+              const raw = parseFloat(m[2][this.state.selectedReadingType])
+              const normalized = raw / 100
+              console.log(
+                'TYPE: ',
+                this.state.selectedReadingType,
+                'RAW: ',
+                raw,
+                'NORMALIZED: ',
+                normalized
+              )
+              return normalized
+            }}
+            radius={Number(this.state.radius)}
+            blur={Number(this.state.blur)}
+            max={Number.parseFloat(this.state.max)}
+          />
           <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
         </Map>
-        <input
-          type="button"
-          value="Toggle Map"
-          onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
-        />
-        <input
-          type="button"
-          value="Toggle Layer"
-          onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}
-        />
-        <input
-          type="button"
-          value="Toggle Limited Data"
-          onClick={this.toggleLimitedAddressPoints.bind(this)}
-        />
-
+        <select
+          onChange={e =>
+            this.setState({ selectedReadingType: e.currentTarget.value })
+          }
+        >
+          <option value="temp">Temperature</option>
+          <option value="hum">Humidity</option>
+          <option value="dust">Dust</option>
+          <option value="pres">Pressure</option>
+        </select>
         <div>
           Radius
           <input
             type="range"
             min={1}
-            max={40}
+            max={100}
             value={this.state.radius}
-            onChange={(e) => this.setState({ radius: e.currentTarget.value })}
-          /> {this.state.radius}
+            onChange={e => this.setState({ radius: e.currentTarget.value })}
+          />{' '}
+          {this.state.radius}
         </div>
 
         <div>
           Blur
           <input
             type="range"
-            min={1}
-            max={20}
+            min={5}
+            max={100}
             value={this.state.blur}
-            onChange={(e) => this.setState({ blur: e.currentTarget.value })}
-          /> {this.state.blur}
+            onChange={e => this.setState({ blur: e.currentTarget.value })}
+          />{' '}
+          {this.state.blur}
         </div>
 
         <div>
           Max
           <input
             type="range"
-            min={0.1}
+            min={0.01}
             max={3}
-            step={0.1}
+            step={0.01}
             value={this.state.max}
-            onChange={(e) => this.setState({ max: e.currentTarget.value })}
-          /> {this.state.max}
+            onChange={e => this.setState({ max: e.currentTarget.value })}
+          />{' '}
+          {this.state.max}
         </div>
       </div>
-    );
+    )
   }
-
 }
 
-render(<MapExample />, document.getElementById('app'));
+render(<MapExample />, document.getElementById('app'))
