fa6a162dca5e594923b337f100ce4b315463ba07
Axel Anderson <axel@pillar.tech>
2019-03-19 15:53:17 -0400

    toggle inverted gradient

--- example/index.js
+++ example/index.js
@@ -4,13 +4,57 @@ import { Map, TileLayer } from 'react-leaflet'
 import HeatmapLayer from '../src/HeatmapLayer'
 import { addressPoints } from './realworld.10000.js'
 
+const COLORS = {
+  WHITE: '#ffffff',
+  BLUE: '#357edd',
+  DARK_BLUE: '#00449E',
+  NAVY: '#001B44',
+  GREEN: '#19a974',
+  DARK_GREEN: '#137752',
+  RED: '#FF4136',
+  DARK_RED: '#E7040F',
+  LIGHT_RED: '#FF725C',
+  LIGHT_GREEN: '#9EEBCF',
+  LIGHT_BLUE: '#96CCFF',
+  ORANGE: '#F48120',
+  PURPLE: '#A463F2',
+  YELLOW: '#FFD700',
+  GOLD: '#FFB700',
+  BLACK: '#000000',
+  BLACK_80: 'rgba(0, 0, 0, 0.8)',
+  GRAY: '#777777',
+  MOON_GRAY: '#CCCCCC',
+  LIGHT_GRAY: '#EEEEEE',
+  LIGHT_SILVER: '#AAAAAA',
+  TRANSPARENT: 'transparent'
+}
+
+const gradient = {
+  0.0: COLORS.DARK_BLUE,
+  0.25: COLORS.BLUE,
+  0.5: COLORS.YELLOW,
+  0.7: COLORS.ORANGE,
+  0.85: COLORS.LIGHT_RED,
+  '1.0': COLORS.RED
+}
+
+const gradientReversed = {
+  0.0: COLORS.RED,
+  0.25: COLORS.LIGHT_RED,
+  0.5: COLORS.ORANGE,
+  0.7: COLORS.YELLOW,
+  0.85: COLORS.BLUE,
+  '1.0': COLORS.DARK_BLUE
+}
+
 class MapExample extends React.Component {
   state = {
     addressPoints,
     radius: 20,
     blur: 8,
     max: 0.02,
-    selectedReadingType: 'temp'
+    selectedReadingType: 'temp',
+    gradient
   }
 
   /**
@@ -18,40 +62,6 @@ class MapExample extends React.Component {
    */
 
   render() {
-    const COLORS = {
-      WHITE: '#ffffff',
-      BLUE: '#357edd',
-      DARK_BLUE: '#00449E',
-      NAVY: '#001B44',
-      GREEN: '#19a974',
-      DARK_GREEN: '#137752',
-      RED: '#FF4136',
-      DARK_RED: '#E7040F',
-      LIGHT_RED: '#FF725C',
-      LIGHT_GREEN: '#9EEBCF',
-      LIGHT_BLUE: '#96CCFF',
-      ORANGE: '#F48120',
-      PURPLE: '#A463F2',
-      YELLOW: '#FFD700',
-      GOLD: '#FFB700',
-      BLACK: '#000000',
-      BLACK_80: 'rgba(0, 0, 0, 0.8)',
-      GRAY: '#777777',
-      MOON_GRAY: '#CCCCCC',
-      LIGHT_GRAY: '#EEEEEE',
-      LIGHT_SILVER: '#AAAAAA',
-      TRANSPARENT: 'transparent'
-    }
-
-    const gradient = {
-      0.0: COLORS.DARK_BLUE,
-      0.25: COLORS.BLUE,
-      0.5: COLORS.YELLOW,
-      0.7: COLORS.ORANGE,
-      0.85: COLORS.RED,
-      '1.0': COLORS.DARK_RED
-    }
-
     return (
       <div>
         <Map center={[0, 0]} zoom={13}>
@@ -61,7 +71,7 @@ class MapExample extends React.Component {
             points={this.state.addressPoints}
             longitudeExtractor={m => m[1]}
             latitudeExtractor={m => m[0]}
-            gradient={gradient}
+            gradient={this.state.gradient}
             intensityExtractor={m => {
               const raw = parseFloat(m[2][this.state.selectedReadingType])
               const normalized = raw / 100
@@ -94,6 +104,20 @@ class MapExample extends React.Component {
           <option value="dust">Dust</option>
           <option value="pres">Pressure</option>
         </select>
+        <div>
+          Invert Gradient
+          <input
+            style={{ marginLeft: 10 }}
+            type="checkbox"
+            onChange={e => {
+              if (e.target.checked) {
+                this.setState({ gradient: gradientReversed })
+              } else {
+                this.setState({ gradient })
+              }
+            }}
+          />
+        </div>
         <div>
           Radius
           <input
